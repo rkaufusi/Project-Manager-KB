6253bec4eb85e6a81d68ee7de1e2eb62b614fac3
rkaufusi <rhettkaufusi@gmail.com>
2022-02-25 22:42:43 -0800

    PUT

--- server/database.sql
+++ server/database.sql
@@ -2,7 +2,6 @@ CREATE DATABASE kanBanPM;
 
 CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
     description VARCHAR(255)
 );
 CREATE TABLE doing(
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import {json, urlencoded} from 'body-parser'
-const pool = require("db")
+const pool = require("./db")
 
 const app = express()
 
@@ -14,15 +14,28 @@ app.use(urlencoded({extended: true}))
 app.listen(PORT, () => {
     console.log(`Connected on port ${PORT}`)
 })
-
-app.get("/", (_, response) => {
+/*
+app.get("/todos", (_, response) => {
     response.send("Hello World")
 })
-
+*/
 //Routes
 
 //getAll
 //create
+
+app.post("/todos", async (req, res) => {
+    try {
+        const {description} = req.body
+        console.log(description)
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", [description])
+
+        res.json(newTodo)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 //update
 //delete
 
