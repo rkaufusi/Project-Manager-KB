e31d186c84146bb6bc8698d6d88252bc7b0a1e63
rkaufusi <rhettkaufusi@gmail.com>
2022-03-01 22:09:17 -0800

    db

--- server/db.ts
+++ server/db.ts
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool
 
 const pool = new Pool({
     user: "postgres",
-    password: "FamilyGuy1",
+    //password:  ,
     host: "localhost",
     port: 5432,
     database: "kanBanPM",
